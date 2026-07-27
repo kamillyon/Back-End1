5a6ee0ee6d7a795e3d5a64a78dfb82e70620c9e5
Kamilly <kakalrrosa@gmail.com>
2026-07-27 12:01:57 -0300

    Add files via upload

---
+++ JS - Backend_Kamilly/script.js
@@ -0,0 +1,44 @@
+//DECLARAÇÃO DE VARIÁVEIS EM JAVASCRIPT
+//Em JavaScript, existem três formas de declarar Variáveis:
+
+//1. Usando var (forma mais antiga)
+var idade = 25;
+var nome = "Maria";
+var estudante = true; //No sistema binário vale 1
+
+//2. Usando let (introduzido no ES6)
+let idade = 25;
+let nome = "Maria";
+let estudante = true;
+
+//3. Usando const (para valores constantes (que não mudam))
+const PI = 3.14159;
+const NOME_EMPRESA = "TechSolutions";
+
+
+//TIPOS DE DADOS EM JAVASCRIPT
+//1. Tipos Primitivos
+Number (número)
+//Representa tanto números inteiros quanto decimais
+let idade = 25; //Número inteiro
+let altura = 1.75; //Número decimal
+let temperatura = -5;
+
+String (texto)
+//Representa sequências de caracteres 
+let nome = 'João';
+let sobrenome = "Silva";
+let endereco = "Rua das flores, 123";
+
+Boolean (Booleano)
+//Representa valores lógicos: verdadeiro (true) ou falso (false)
+let estudante = true;
+let  = false;
+
+Undefined 
+//Representa uma variável que foi declarada, mas não recebeu um valor definido
+let cidade; //valor é undefined
+
+Null
+//Representa a ausência intencional de valor
+let telefone = null; //Explicitamente sem valor
